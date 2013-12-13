b89d13ad5f35349b6392e90840e0db5d29385426
jamiewallace <wallacejamiec@gmail.com>
2013-12-13 15:30:04 +0000

    carrierwave stuff

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,31 +1,31 @@
-class Ability
-  include CanCan::Ability
+# class Ability
+#   include CanCan::Ability
 
-  def initialize(user)
-    user ||= User.new
+#   def initialize(user)
+#     user ||= User.new
 
-    if user.role == 'admin'
-      can :manage, :all
+#     if user.role == 'admin'
+#       can :manage, :all
     
-    elsif user.role == 'user'
+#     elsif user.role == 'user'
 
-      can :read, :all
+#       can :read, :all
 
-      can :create, Image
-      can :edit, Image do |image|
-        image.user == user
-      end
-      can :update, Post do |image|
-        image.user == user
-      end
-      can :destroy, Post do |image|
-        image.user == user
-      end
+#       can :create, Image
+#       can :edit, Image do |image|
+#         image.user == user
+#       end
+#       can :update, Post do |image|
+#         image.user == user
+#       end
+#       can :destroy, Post do |image|
+#         image.user == user
+#       end
 
-    else
-      can :read, :all
-      can :create, User
+#     else
+#       can :read, :all
+#       can :create, User
 
-    end
-  end
-end
+#     end
+#   end
+# end
