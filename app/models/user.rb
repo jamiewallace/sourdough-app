922476600fc6598e1ebe1c169ee5895ca9a8ab09
jamiewallace <wallacejamiec@gmail.com>
2013-12-14 15:43:05 +0000

    changes to nav bar and stylesheets

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable#, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :first_name, :last_name, :avatar, :first_line_address, :second_line_address, :town_city, :postcode, :country, :description, :lat, :lon, :have_need, :role
