b0822a777445033b43ede4abde82dfa2b27c3464
jamiewallace <wallacejamiec@gmail.com>
2013-12-16 15:17:23 +0000

    map javascript

---
+++ app/assets/javascripts/maps.js
@@ -0,0 +1,55 @@
+$(function(){
+  var map;
+
+  function openWindow(marker, contentString){
+    var infowindow = new google.maps.InfoWindow({
+      content: contentString
+    });
+    infowindow.open(map, marker)
+  }
+
+  function renderUsersPosition(){
+    $.each(window.users, function(index, user){
+      var marker = new google.maps.Marker({
+          map: map,
+          position: new google.maps.LatLng(user.lat, user.lon)
+      });
+
+      google.maps.event.addListener(marker, 'click', function() {
+        string = "<img src='"+user.avatar.url+"' width='50'><p>"+user.first_name+"</p>"
+        openWindow(marker, string)
+      });
+    })
+  }
+
+  function codeAddress(address) {
+    geocoder = new google.maps.Geocoder();
+    geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        console.log(results[0].geometry.location)
+
+      } else {
+        alert("Geocode was not successful for the following reason: " + status);
+      }
+    });
+  }
+
+
+  function initialize() {
+    var mapOptions = {
+      zoom: 12,
+      center: new google.maps.LatLng(51.508315, -0.103683)
+    };
+    map = new google.maps.Map(document.getElementById('map-canvas-global'),
+        mapOptions);
+    renderUsersPosition()
+  }
+
+  initialize();
+  
+  
+})
+
+
+
+
