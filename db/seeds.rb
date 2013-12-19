f2f282295d780846e12d5474830c264b3b4d5fd9
jamiewallace <wallacejamiec@gmail.com>
2013-12-19 22:25:47 +0000

    show page styling, image uploads, seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,17 +14,32 @@ file9 = File.open(File.join(Rails.root,"/app/assets/images/sd5.jpg"))
 file10 = File.open(File.join(Rails.root,"/app/assets/images/sd6.jpeg"))
 file11 = File.open(File.join(Rails.root,"/app/assets/images/sd7.jpg"))
 file12 = File.open(File.join(Rails.root,"/app/assets/images/sd8.jpeg"))
+file13 = File.open(File.join(Rails.root,"/app/assets/images/gerry.jpg"))
+file14 = File.open(File.join(Rails.root,"/app/assets/images/jonchambers.jpg"))
+file15 = File.open(File.join(Rails.root,"/app/assets/images/david.jpg"))
+file16 = File.open(File.join(Rails.root,"/app/assets/images/julien.jpg"))
 
 
 #remote_image_url
 
-user1 = User.create!(username: "NeNe", first_name: "Neha", last_name: "Shah", avatar: file1, first_line_address: "9 Back Hill", second_line_address: "", town_city: "London", postcode: "EC1R 5EN", country: "UK", description: "CSS supremo", have_need: 'need', email: "neshah07@gmail.com", password: "password", password_confirmation: "password", role: "admin")
+user1 = User.create!(username: "NeNe", first_name: "Neha", last_name: "Shah", avatar: file1, first_line_address: "9 Back Hill", second_line_address: "", town_city: "London", postcode: "EC1R 5EN", country: "UK", description: "If you give me some sourdough starter I'll be eternally grateful.  I need lessons too!", have_need: 'need', email: "neshah07@gmail.com", password: "password", password_confirmation: "password", role: "admin")
 
-user2 = User.create!(username: "Jamtista", first_name: "Jamie", last_name: "Wallace", avatar: file2, first_line_address: "12 Aquinas Street", second_line_address: "SE1 8AE", town_city: "London", postcode: "", country: "UK", description: "Professional coder", have_need: 'need', email: "wallacejamiec@gmail.com", password: "password", password_confirmation: "password", role: "admin")
+user2 = User.create!(username: "Jamtista", first_name: "Jamie", last_name: "Wallace", avatar: file2, first_line_address: "12 Aquinas Street", second_line_address: "SE1 8AE", town_city: "London", postcode: "", country: "UK", description: "Desperately seeking a sourdough starter with a good sense of humour.", have_need: 'need', email: "wallacejamiec@gmail.com", password: "password", password_confirmation: "password", role: "admin")
 
-user3 = User.create!(username: "HamHam", first_name: "Alex", last_name: "Hamlin", avatar: file3, first_line_address: "13 Eyre Street Hill", second_line_address: "", town_city: "London", postcode: "EC1R 5ET", country: "UK", description: "A shit baker", have_need: 'have', email: "alexham100@aol.com", password: "password", password_confirmation: "password")
+user3 = User.create!(username: "HamHam", first_name: "Alex", last_name: "Hamlin", avatar: file3, first_line_address: "13 Eyre Street Hill", second_line_address: "", town_city: "London", postcode: "EC1R 5ET", country: "UK", description: "Happy to share my yeast culture with all and sundry, especially the ladies!", have_need: 'have', email: "alexham100@aol.com", password: "password", password_confirmation: "password")
 
-user4 = User.create!(username: "DamDam", first_name: "Adam", last_name: "Buchan", avatar: file4, first_line_address: "4-5 Bonhill Street", second_line_address: "", town_city: "London", postcode: "EC2A 4BX", country: "UK", description: "A master baker", have_need: 'have', email: "adam.buchan@gmail.com", password: "password", password_confirmation: "password")
+user4 = User.create!(username: "DamDam", first_name: "Adam", last_name: "Buchan", avatar: file4, first_line_address: "4-5 Bonhill Street", second_line_address: "", town_city: "London", postcode: "EC2A 4BX", country: "UK", description: "I've been known to bake a mean loaf of bread in my time, and can share my skills...", have_need: 'have', email: "adam.buchan@gmail.com", password: "password", password_confirmation: "password")
+
+user5 = User.create!(username: "Davido", first_name: "David", last_name: "Grilli", avatar: file15, first_line_address: "", second_line_address: "", town_city: "Caracas", postcode: "", country: "Venezuela", description: "The best bread maker in Venezuela who would love to share his knowledge with aspiring bakers.", have_need: 'need', email: "david@david.com", password: "password", password_confirmation: "password")
+
+user6 = User.create!(username: "JonJon", first_name: "Jon", last_name: "Chambers", avatar: file14, first_line_address: "", second_line_address: "", town_city: "Sheffield", postcode: "", country: "UK", description: "An aspiring bread maker from Sheffield who would like to get hold of some starter locally.", have_need: 'need', email: "jon@jon.com", password: "password", password_confirmation: "password")
+
+user7 = User.create!(username: "Gerry", first_name: "Geraud", last_name: "Mathe", avatar: file13, first_line_address: "", second_line_address: "", town_city: "Paris", postcode: "", country: "France", description: "I think baguette is the best bread in the world, but I'm prepared to try this sourdough thing.", have_need: 'need', email: "gerry@gerry.com", password: "password", password_confirmation: "password")
+
+user7 = User.create!(username: "Jools", first_name: "Julien", last_name: "Longsurname", avatar: file16, first_line_address: "", second_line_address: "", town_city: "Bordeaux", postcode: "", country: "France", description: "J'aime beaucoup du pain", have_need: 'need', email: "julien@julien.com", password: "password", password_confirmation: "password")
+
+
+
 
 image1= Image.create!(user_id: 1, image: file5)
 image2= Image.create!(user_id: 1, image: file6)
